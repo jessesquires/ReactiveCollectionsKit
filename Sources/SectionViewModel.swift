9aa39a983995e2d61f5268b68d9ad9fd05e76568
Jesse Squires <jesse@jessesquires.com>
2019-10-24 06:39:45 -0700

    Add initial SectionViewModel

---
+++ Sources/SectionViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  Created by Jesse Squires
+//  https://www.jessesquires.com
+//
+//
+//  Documentation
+//  https://jessesquires.github.io/DiffableCollectionsKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/DiffableCollectionsKit
+//
+//
+//  License
+//  Copyright © 2019-present Jesse Squires
+//  Released under an MIT license: https://opensource.org/licenses/MIT
+//
+
+import Foundation
+
+public struct SectionViewModel {
+
+    public let cellViewModels: [CellViewModel]
+
+    public let headerViewModel: SupplementaryViewModel?
+
+    public let footerViewModel: SupplementaryViewModel?
+
+    init(cells: [CellViewModel],
+         header: SupplementaryViewModel? = nil,
+         footer: SupplementaryViewModel? = nil) {
+        self.cellViewModels = cells
+        self.headerViewModel = header
+        self.footerViewModel = footer
+    }
+}
+
