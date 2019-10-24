3700c37decd9a77349a664250a19dd3ebc55dbfa
Jesse Squires <jesse@jessesquires.com>
2019-10-23 20:09:00 -0700

    Add empty Xcode project

---
+++ Sources/DiffableCollectionsKit.h
@@ -0,0 +1,25 @@
+//
+//  Created by Jesse Squires
+//  https://www.jessesquires.com
+//
+//
+//  Documentation
+//  https://jessesquires.github.io/DiffableCollectionsKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/DiffableCollectionsKit
+//
+//
+//  License
+//  Copyright © 2019-present Jesse Squires
+//  Released under an MIT license: https://opensource.org/licenses/MIT
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DiffableCollectionsKit.
+FOUNDATION_EXPORT double DiffableCollectionsKitVersionNumber;
+
+//! Project version string for DiffableCollectionsKit.
+FOUNDATION_EXPORT const unsigned char DiffableCollectionsKitVersionString[];
