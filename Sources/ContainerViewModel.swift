e88031882cfec9e0ea1f82d63f0ce91f144f25a8
Jesse Squires <jesse@jessesquires.com>
2019-10-24 06:43:41 -0700

    add initial ContainerViewModel

---
+++ Sources/ContainerViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  Created by Jesse Squires
+//  https://www.jessesquires.com
+//
+//
+//  Documentation
+//  https://jessesquires.github.io/DiffableCollectionsKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/DiffableCollectionsKit
+//
+//
+//  License
+//  Copyright © 2019-present Jesse Squires
+//  Released under an MIT license: https://opensource.org/licenses/MIT
+//
+
+import Foundation
+
+public struct ContainerViewModel {
+
+    public let sections: [SectionViewModel]
+
+    public subscript (indexPath: IndexPath) -> CellViewModel {
+        self.sections[indexPath.section].cellViewModels[indexPath.item]
+    }
+}
