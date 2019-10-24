152476ee85b3c35350e0810dc73013318e2fab1a
Jesse Squires <jesse@jessesquires.com>
2019-10-23 23:54:41 -0700

    Remove associatedtype from ReusableViewProtocol
    This isn't going to work, because then the protocol can only be used as a generic constraint, which we don't want.
    Such a constraint would require that all cells be the same type, and we want to be able to have different cell types.
    
    Plus, maybe cells shouldn't know their container anyway.

--- Sources/ReusableViewProtocol.swift
+++ Sources/ReusableViewProtocol.swift
@@ -20,41 +20,17 @@ import UIKit
 
 /// This protocol unifies cells and supplementary views for
 /// collections and tables by providing a common interface.
-public protocol ReusableViewProtocol {
-
-    // MARK: Associated types
-
-    /// The container view of the cell or supplementary view.
-    ///
-    /// For example, for `UICollectionViewCell` this is `UICollectionView`,
-    /// and for `UITableViewCell` this is `UITableView`.
-    associatedtype ContainerView: UIView & CellContainerViewProtocol
-
-    // MARK: Properties
+public protocol ReusableViewProtocol: AnyObject {
 
     /// A string that identifies the purpose of the view.
     var reuseIdentifier: String? { get }
-
-    // MARK: Methods
 
     /// Performs any clean up necessary to prepare the view for use again.
     func prepareForReuse()
 }
 
-extension UICollectionReusableView: ReusableViewProtocol {
+extension UICollectionReusableView: ReusableViewProtocol { }
 
-    /// :nodoc:
-    public typealias ContainerView = UICollectionView
-}
+extension UITableViewCell: ReusableViewProtocol { }
 
-extension UITableViewCell: ReusableViewProtocol {
-
-    /// :nodoc:
-    public typealias ContainerView = UITableView
-}
-
-extension UITableViewHeaderFooterView: ReusableViewProtocol {
-
-    /// :nodoc:
-    public typealias ContainerView = UITableView
-}
+extension UITableViewHeaderFooterView: ReusableViewProtocol { }
