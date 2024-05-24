bc63529045a8ace0c1cc9558a21b889e831a7878
Jesse Squires <jesse@jessesquires.com>
2024-05-24 13:24:48 -0700

    rewrite and optimize supplementary view reloading

--- Sources/DiffableDataSource.swift
+++ Sources/DiffableDataSource.swift
@@ -35,6 +35,10 @@ final class DiffableDataSource: UICollectionViewDiffableDataSource<AnyHashable, 
 
     typealias SnapshotCompletion = () -> Void
 
+    // Avoid retaining the collection view, we know it is owned and kept alive by the driver.
+    // Thus, unowned is safe here.
+    private unowned let _collectionView: UICollectionView
+
     private let _diffOnBackgroundQueue: Bool
 
     private lazy var _diffingQueue = DispatchQueue(
@@ -51,6 +55,7 @@ final class DiffableDataSource: UICollectionViewDiffableDataSource<AnyHashable, 
         cellProvider: @escaping DiffableDataSource.CellProvider,
         supplementaryViewProvider: @escaping DiffableDataSource.SupplementaryViewProvider
     ) {
+        self._collectionView = view
         self._diffOnBackgroundQueue = diffOnBackgroundQueue
         super.init(collectionView: view, cellProvider: cellProvider)
         self.supplementaryViewProvider = supplementaryViewProvider
@@ -81,28 +86,46 @@ final class DiffableDataSource: UICollectionViewDiffableDataSource<AnyHashable, 
         // Build initial destination snapshot, then make adjustments below.
         var destinationSnapshot = DiffableSnapshot(viewModel: destination)
 
-        // Apply item reloads, then section reloads.
-        // Reloading a section is required to properly reload headers, footers, and other supplementary views.
-        // This 2-step process is necessary to preserve collection view animations
-        // and prevent UIKit data source internal inconsistency exceptions.
+        // Apply item reconfigures, then supplementary view reconfigures.
+        // Note: "reconfigure" is a lighter weight, more efficient "reload".
         //
-        // If we only reloaded a section (instead of reloading items first),
-        // there are 2 problems:
-        //   1. item updates would not animate correctly, because the whole section is reloaded.
-        //   2. item inserts/deletes could trigger an internal inconsistency exception.
+        // Unfortunately headers, footers, and other supplementary views
+        // do not get properly reconfigured or reloaded if they display dynamic data.
+        // This is a shortcoming in UIKit. The APIs we need to do this do not exist.
+        // Ideally, `DiffableDataSource` would offer the same APIs for supplementary views
+        // as it does for items and sections.
+        //
+        // Generally speaking, hard-reloading an entire section is the only way to get
+        // headers, footers, and other supplementary views to reload/reconfigure.
+        // That sucks. Luckily, we can do better.
+        //
+        // Below is a 2-step process that is necessary to preserve collection view animations
+        // and prevent UIKit data source internal inconsistency exceptions.
 
         // Find and perform item (cell) updates first.
-        let itemsToReload = self._findItemsToReload(from: source, to: destination)
-        destinationSnapshot.reconfigureItems(itemsToReload)
+        // Add the item reconfigure updates to the snapshot.
+        let itemsToReconfigure = self._findItemsToReconfigure(from: source, to: destination)
+        destinationSnapshot.reconfigureItems(itemsToReconfigure)
 
-        // Apply snapshot with item reload updates.
+        // Apply the snapshot with item reconfigure updates.
         self._applyDiffSnapshot(destinationSnapshot, animated: animated, completion: UncheckedCompletion {
 
-            // Once the snapshot with item reloads is applied, find and apply section reloads, if needed.
-            // This is necessary to update SUPPLEMENTARY VIEWS ONLY.
-            // Supplementary views do not get reloaded / reconfigured automatically when they change.
-            // To trigger updates on supplementary views, the section must be reloaded.
-            // Yes, this kinda sucks.
+            // Once the snapshot with item reconfigures is applied,
+            // we need to find and apply supplementary view reconfigures, if needed.
+            //
+            // This is necessary to update all headers, footers, and supplementary views.
+            // Per notes above, supplementary views do not get reloaded / reconfigured
+            // automatically by `DiffableDataSource` when they change.
+            //
+            // To trigger updates on supplementary views with the existing APIs,
+            // the entire section must be reloaded. Yes, that sucks. We don't want to do that.
+            // That causes all items in the section to be hard-reloaded, too.
+            // Aside from the performance impact, doing that results in an ugly UI "flash"
+            // for all item cells in the collection. Gross.
+            //
+            // However, we can actually do much better than a hard reload!
+            // Instead of reloading the entire section, we can find and compare
+            // the supplementary views and manually reconfigure them if they changed.
             //
             // NOTE: this only matters if supplementary views are not static.
             // That is, if they reflect data in the data source.
@@ -110,108 +133,110 @@ final class DiffableDataSource: UICollectionViewDiffableDataSource<AnyHashable, 
             // For example, a header with a fixed title (e.g. "My Items") will NOT need to be reloaded.
             // However, a header that displays changing data WILL need to be reloaded.
             // (e.g. "My 10 Items")
-            let sectionsToReload = self._findSectionsToReload(from: source, to: destination)
 
-            // If no sections need reloading, we're done.
-            guard sectionsToReload.isNotEmpty else {
-                completion?()
-                return
-            }
+            // Check all the supplementary views and reconfigure them, if needed.
+            self._reconfigureSupplementaryViewsIfNeeded(from: source, to: destination)
 
-            // Apply final section updates
-            destinationSnapshot.reloadSections(sectionsToReload)
-            self._applyDiffSnapshot(destinationSnapshot, animated: animated, completion: UncheckedCompletion(completion))
+            // Finally, we're done and can call completion.
+            completion?()
         })
     }
 
-    private func _findItemsToReload(
+    private func _findItemsToReconfigure(
         from source: CollectionViewModel,
         to destination: CollectionViewModel
     ) -> [UniqueIdentifier] {
-        // Determine which items need to be reloaded.
         let allSourceCells = source.allCellsByIdentifier
         let allDestinationCells = destination.allCellsByIdentifier
 
-        var itemsToReload = [UniqueIdentifier]()
+        var itemsToReconfigure = [UniqueIdentifier]()
 
         for (cellId, destinationCell) in allDestinationCells {
             let sourceCell = allSourceCells[cellId]
 
             // If this cell exist in the source, and it has changed, then reload it.
             if destinationCell != sourceCell {
-                itemsToReload.append(cellId)
+                itemsToReconfigure.append(cellId)
             }
         }
 
-        return itemsToReload
+        return itemsToReconfigure
     }
 
-    private func _findSectionsToReload(
+    private func _reconfigureSupplementaryViewsIfNeeded(
         from source: CollectionViewModel,
         to destination: CollectionViewModel
-    ) -> [UniqueIdentifier] {
+    ) {
         let allSourceSections = source.allSectionsByIdentifier
 
-        // Only get sections that have supplementary views.
-        let allDestinationSections = destination.allSectionsByIdentifier.filter { _, value in
-            value.hasSupplementaryViews
-        }
+        for sectionIndex in 0..<destination.sections.count {
+            let destinationSection = destination.sections[sectionIndex]
 
-        // If no sections have supplementary views, there's nothing to do.
-        guard allDestinationSections.isNotEmpty else {
-            return []
-        }
-
-        var sectionsToReload = [UniqueIdentifier]()
-
-        // As soon as we find 1 supplementary view in a section that needs reloading,
-        // we can mark the whole section for reload and exit early.
-        for (sectionId, destinationSection) in allDestinationSections {
-            // If this section does not exist in the source, then it is newly inserted.
-            // Thus, nothing to do.
-            guard let sourceSection = allSourceSections[sectionId] else {
+            // If this section does not have any supplementary views, skip it.
+            guard destinationSection.hasSupplementaryViews else {
                 continue
             }
 
-            // If this section exists in the source,
-            // and it has changed its SUPPLEMENTARY VIEWS ONLY,
-            // then reload the section.
-            // First, check headers and footers.
-            if destinationSection.header != sourceSection.header {
-                sectionsToReload.append(sectionId)
+            // If this section does not exist in the source,
+            // then it is newly inserted. Thus, nothing to do.
+            guard let sourceSection = allSourceSections[destinationSection.id] else {
                 continue
             }
 
-            if destinationSection.footer != sourceSection.footer {
-                sectionsToReload.append(sectionId)
-                continue
+            // This section exists in the source.
+            // Check if it has changed its supplementary views.
+
+            // First, check and reconfigure header.
+            if let destinationHeader = destinationSection.header,
+               let sourceHeader = sourceSection.header,
+               destinationHeader != sourceHeader {
+                self._reconfigureSupplementaryView(
+                    model: destinationHeader,
+                    item: 0,
+                    section: sectionIndex
+                )
             }
 
-            // Next, check all supplementary views.
+            // Second, check and reconfigure footer.
+            if let destinationFooter = destinationSection.footer,
+               let sourceFooter = sourceSection.footer,
+               destinationFooter != sourceFooter {
+                self._reconfigureSupplementaryView(
+                    model: destinationFooter,
+                    item: 0,
+                    section: sectionIndex
+                )
+            }
+
+            // Third, check all supplementary views.
             let allSourceSectionSupplementaryViews = sourceSection.allSupplementaryViewsByIdentifier
 
-            for destinationView in destinationSection.supplementaryViews {
-                // If this view does not exist in the source, then it is newly added.
-                // Thus, nothing to do.
+            for viewIndex in 0..<destinationSection.supplementaryViews.count {
+                let destinationView = destinationSection.supplementaryViews[viewIndex]
+
+                // If this view does not exist in the source,
+                // then it is newly added. Thus, nothing to do.
                 guard let sourceView = allSourceSectionSupplementaryViews[destinationView.id] else {
                     continue
                 }
 
-                // After finding one view that needs reloading, we can stop,
-                // because we have to reload the whole section anyway.
+                // Check and reconfigure supplementary view.
                 if destinationView != sourceView {
-                    sectionsToReload.append(sectionId)
-                    break
+                    self._reconfigureSupplementaryView(
+                        model: destinationView,
+                        item: viewIndex,
+                        section: sectionIndex
+                    )
                 }
             }
         }
+    }
 
-        // If no section changes, ignore and call completion
-        guard sectionsToReload.isNotEmpty else {
-            return []
+    private func _reconfigureSupplementaryView(model: AnySupplementaryViewModel, item: Int, section: Int) {
+        let indexPath = IndexPath(item: item, section: section)
+        if let view = self._collectionView.supplementaryView(forElementKind: model._kind, at: indexPath) {
+            model.configure(view: view)
         }
-
-        return sectionsToReload
     }
 
     // MARK: Diffing
