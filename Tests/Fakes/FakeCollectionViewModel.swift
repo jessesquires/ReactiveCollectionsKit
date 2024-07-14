50da58da34f164d96f7255a59fa11a8b406ee362
nuomi1 <nuomi1@qq.com>
2024-07-14 23:34:21 +0800

    feat: pass generator to _fakeCellViewModel

--- Tests/Fakes/FakeCollectionViewModel.swift
+++ Tests/Fakes/FakeCollectionViewModel.swift
@@ -20,16 +20,29 @@ extension XCTestCase {
     @MainActor
     func fakeCollectionViewModel(
         id: String = .random,
+        sectionId: (Int) -> String = { _ in .random },
+        cellId: (Int, Int) -> String = { _, _ in .random },
+        supplementaryViewId: (Int, Int) -> String = { _, _ in .random },
         numSections: Int = Int.random(in: 2...15),
         numCells: Int? = nil,
         useCellNibs: Bool = false,
+        includeHeader: Bool = false,
+        includeFooter: Bool = false,
+        includeSupplementaryViews: Bool = false,
         expectDidSelectCell: Bool = false,
         expectConfigureCell: Bool = false
     ) -> CollectionViewModel {
-        let sections = (0..<numSections).map { _ in
+        let sections = (0..<numSections).map { sectionIndex in
             self.fakeSectionViewModel(
+                id: sectionId(sectionIndex),
+                cellId: cellId,
+                supplementaryViewId: supplementaryViewId,
+                sectionIndex: sectionIndex,
                 numCells: numCells ?? Int.random(in: 3...20),
                 useCellNibs: useCellNibs,
+                includeHeader: includeHeader,
+                includeFooter: includeFooter,
+                includeSupplementaryViews: includeSupplementaryViews,
                 expectDidSelectCell: expectDidSelectCell,
                 expectConfigureCell: expectConfigureCell
             )
@@ -40,14 +53,20 @@ extension XCTestCase {
     @MainActor
     func fakeSectionViewModel(
         id: String = .random,
+        cellId: (Int, Int) -> String = { _, _ in .random },
+        supplementaryViewId: (Int, Int) -> String = { _, _ in .random },
+        sectionIndex: Int = 0,
         numCells: Int = Int.random(in: 1...20),
         useCellNibs: Bool = false,
         includeHeader: Bool = false,
         includeFooter: Bool = false,
+        includeSupplementaryViews: Bool = false,
         expectDidSelectCell: Bool = false,
         expectConfigureCell: Bool = false
     ) -> SectionViewModel {
         let cells = self.fakeCellViewModels(
+            id: cellId,
+            sectionIndex: sectionIndex,
             count: numCells,
             useNibs: useCellNibs,
             expectDidSelectCell: expectDidSelectCell,
@@ -55,25 +74,32 @@ extension XCTestCase {
         )
         let header = includeHeader ? FakeHeaderViewModel() : nil
         let footer = includeFooter ? FakeFooterViewModel() : nil
+        let supplementaryViews = includeSupplementaryViews
+            ? (0..<numCells).map { cellIndex in FakeSupplementaryViewModel(title: supplementaryViewId(sectionIndex, cellIndex)) }
+            : []
         return SectionViewModel(
             id: "section_\(id)",
             cells: cells,
             header: header,
-            footer: footer
+            footer: footer,
+            supplementaryViews: supplementaryViews
         )
     }
 
     @MainActor
     func fakeCellViewModels(
+        id: (Int, Int) -> String = { _, _ in .random },
+        sectionIndex: Int = 0,
         count: Int = Int.random(in: 3...20),
         useNibs: Bool = false,
         expectDidSelectCell: Bool = false,
         expectConfigureCell: Bool = false
     ) -> [AnyCellViewModel] {
         var cells = [AnyCellViewModel]()
-        for index in 0..<count {
+        for cellIndex in 0..<count {
             let model = self._fakeCellViewModel(
-                index: index,
+                id: id(sectionIndex, cellIndex),
+                cellIndex: cellIndex,
                 useNibs: useNibs,
                 expectDidSelectCell: expectDidSelectCell,
                 expectConfigureCell: expectConfigureCell
@@ -85,26 +111,27 @@ extension XCTestCase {
 
     @MainActor
     private func _fakeCellViewModel(
-        index: Int,
+        id: String,
+        cellIndex: Int,
         useNibs: Bool,
         expectDidSelectCell: Bool,
         expectConfigureCell: Bool
     ) -> AnyCellViewModel {
         if useNibs {
-            var viewModel = FakeCellNibViewModel()
+            var viewModel = FakeCellNibViewModel(id: id)
             viewModel.expectationDidSelect = self._cellDidSelectExpectation(expect: expectDidSelectCell, id: viewModel.id)
             viewModel.expectationConfigureCell = self._cellConfigureExpectation(expect: expectConfigureCell, id: viewModel.id)
             return viewModel.eraseToAnyViewModel()
         }
 
-        if index.isMultiple(of: 2) {
-            var viewModel = FakeNumberCellViewModel()
+        if cellIndex.isMultiple(of: 2) {
+            var viewModel = FakeNumberCellViewModel(model: .init(id: id))
             viewModel.expectationDidSelect = self._cellDidSelectExpectation(expect: expectDidSelectCell, id: viewModel.id)
             viewModel.expectationConfigureCell = self._cellConfigureExpectation(expect: expectConfigureCell, id: viewModel.id)
             return viewModel.eraseToAnyViewModel()
         }
 
-        var viewModel = FakeTextCellViewModel()
+        var viewModel = FakeTextCellViewModel(model: .init(text: id))
         viewModel.expectationDidSelect = self._cellDidSelectExpectation(expect: expectDidSelectCell, id: viewModel.id)
         viewModel.expectationConfigureCell = self._cellConfigureExpectation(expect: expectConfigureCell, id: viewModel.id)
         return viewModel.eraseToAnyViewModel()
